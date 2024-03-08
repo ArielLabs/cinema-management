8ee090c5e6d239e6e1281020deb8dc668a8fc1d5
ArielLabs <arielassraf@gmail.com>
2024-03-08 14:44:11 +0200

    create two components: UsersPage and SubscriptionsPage

---
+++ client/src/pages/SubscriptionsPage/SubscriptionsPage.jsx
@@ -0,0 +1,9 @@
+import styles from "./SubscriptionsPage.module.css";
+
+const SubscriptionsPage = () => {
+    return(
+        <div style={{color: "white"}}>Subscriptions Page</div>
+    );
+};
+
+export default SubscriptionsPage;
---
+++ client/src/pages/UsersPage/UsersPage.jsx
@@ -0,0 +1,9 @@
+import styles from "./UsersPage.module.css";
+
+const UsersPage = () => {
+    return(
+        <div style={{color: "white"}}>Users Page</div>
+    );
+};
+
+export default UsersPage;
