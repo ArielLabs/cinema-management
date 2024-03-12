30e5641c6973cd9f6b33e56c16ef23d5126a5b32
ArielLabs <arielassraf@gmail.com>
2024-03-12 23:39:13 +0200

    add nav links to Header component

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,53 @@
+import { NavLink, useNavigate } from "react-router-dom";
+import IconButton from "@mui/material/IconButton";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 import styles from "./Header.module.css";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const logoutHandler = () => {
+    navigate("/login");
+  };
+
   return (
     <div className={styles.header}>
       <div className={styles.toolbar}>
+        <div className={styles.headerUser}>
+          <AccountCircleIcon />
+          <span>Ariel Asraf</span>
+        </div>
         <span className={styles.titleApp}>Cinema</span>
+      </div>
+      <div className={styles.navlinks}>
+        <NavLink
+          to={"users"}
+          className={({ isActive }) =>
+            isActive ? styles.activeLink : styles.link
+          }
+        >
+          Users
+        </NavLink>
+        <NavLink
+          to={"subscriptions"}
+          className={({ isActive }) =>
+            isActive ? styles.activeLink : styles.link
+          }
+        >
+          Subscriptions
+        </NavLink>
+        <NavLink
+          to={"movies"}
+          className={({ isActive }) =>
+            isActive ? styles.activeLink : styles.link
+          }
+        >
+          Movies
+        </NavLink>
+        <IconButton sx={{ color: "white" }} onClick={logoutHandler}>
+          <ExitToAppIcon />
+        </IconButton>
       </div>
     </div>
   );
