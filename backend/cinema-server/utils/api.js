772d001660d8d7e1c96231d33505136bae400900
ArielLabs <arielassraf@gmail.com>
2024-08-25 13:44:22 +0300

    fix: import environment variables to api.js

--- backend/cinema-server/utils/api.js
+++ backend/cinema-server/utils/api.js
@@ -1,4 +1,4 @@
-import { SUBSCRIPTIONS_API_DEV, SUBSCRIPTIONS_API_PROD } from "../environment";
+import { SUBSCRIPTIONS_API_DEV, SUBSCRIPTIONS_API_PROD } from "../environment.js";
 
 export const getSubscriptionsApi = () => {
   return process.env.NODE_ENV === "development"
